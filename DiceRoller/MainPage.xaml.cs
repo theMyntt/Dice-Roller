03e09b4692edd71abe357025e19f9eec59d7fa1d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-03 20:23:18 -0300

    feat: rand function

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -14,5 +14,21 @@ namespace DiceRoller
         {
             InitializeComponent();
         }
+
+        void RandResult(System.Object sender, System.EventArgs e)
+        {
+            Random rand = new Random();
+
+            int selectedNum = (int)SelectDiceFaceNumber.SelectedItem;
+
+            if (SelectDiceFaceNumber.SelectedItem != null)
+            {
+                DiceRollerResult.Text = rand.Next(1, selectedNum + 1).ToString();
+            }
+            else
+            {
+                DisplayAlert("Atenção", "Você precisa selecionar um número primeiro antes de sortear", "Ok");
+            }
+        }
     }
 }
