24fee0078638a5c7b909fc015cd2b5bed113e4c0
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-10 17:46:23 -0300

    chore: best way to make the same thing

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -15,24 +15,18 @@ namespace DiceRoller
         public MainPage()
         {
             InitializeComponent();
+            SelectDiceFaceNumber.SelectedIndex = 0;
         }
 
         void RandResult(System.Object sender, System.EventArgs e)
         {
             Random rand = new Random();
+            int max = (int)SelectDiceFaceNumber.SelectedItem;
 
-            if (index != -1 && SelectDiceFaceNumber.SelectedItem != null)
-            {
-                int min = 1;
-                int max = (int)SelectDiceFaceNumber.SelectedItem;
-
-                int v = rand.Next(min, max + 1);
-                DiceRollerResult.Text = v.ToString();
-            }
-            else
-            {
-                DisplayAlert("Atenção", "Você precisa selecionar um dado primeiro.", "OK");
-            }
+            int v = rand.Next(1, max + 1);
+            DiceRollerResult.Text = v.ToString();
         }
     }
 }
+
+
